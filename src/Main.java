6dc4ac0b9236c01601f1fc57076c4d2956c18170
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-11-23 12:45:25 +0100

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.printf("=== 3.8 Parametry Programu ===\n");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("Liczba parametrów: " + args.length);
+
+        //Wyświetlenie wszystkich przekazanych parametórw
+        System.out.print("Przekazane parametry: ");
+        for(int i=0; i < args.length; i++)
+        {
+            System.out.print(args[i] + "\t");
         }
+
+        System.out.println("\n\n");
     }
+
 }
